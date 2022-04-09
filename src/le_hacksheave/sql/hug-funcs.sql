bfe200d260de9110820d0a7fd6fe823a700b9c99
Maor Kadosh <git@avocadosh.xyz>
2022-04-09 14:09:21 +0300

    i have finally configured sql formatting properly

--- src/le_hacksheave/sql/hug-funcs.sql
+++ src/le_hacksheave/sql/hug-funcs.sql
@@ -2,20 +2,25 @@
 -- :command execute
 -- :result :raw
 -- :doc Delete all rows from Album
-DELETE FROM Album WHERE 1
+DELETE FROM Album
+WHERE 1;
 
 -- :name insert-albums
 -- :command :insert
 -- :result :affected
 -- :doc Insert albums to the Albums table
 INSERT INTO Album (name, artist, cover_art_url)
-VALUES :tuple*:albums
+    VALUES
+        :tuple * :albums;
 
 -- :name random-album
 -- :command :query
 -- :result :one
 -- :doc Pick a random album
-SELECT *
-  FROM Album
- ORDER BY RANDOM()
- LIMIT 1
+SELECT
+    *
+FROM
+    Album
+ORDER BY
+    RANDOM()
+LIMIT 1;
--- src/le_hacksheave/sql/migrations.sql
+++ src/le_hacksheave/sql/migrations.sql
@@ -1,5 +1,17 @@
--- should just be ran manually against the DB
+CREATE TABLE IF NOT EXISTS Genre (
+    name text,
+    url text,
+    id text
+);
 
-CREATE TABLE IF NOT EXISTS Genre (name TEXT, url TEXT, id TEXT);
-CREATE TABLE IF NOT EXISTS Album (name TEXT, artist TEXT, cover_art_url TEXT);
-CREATE TABLE IF NOT EXISTS AlbumGenre (name TEXT, artist TEXT, cover_art_url TEXT);
+CREATE TABLE IF NOT EXISTS Album (
+    name text,
+    artist text,
+    cover_art_url text
+);
+
+CREATE TABLE IF NOT EXISTS AlbumGenre (
+    name text,
+    artist text,
+    cover_art_url text
+);
